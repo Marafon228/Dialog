a5bb551bf6d1db349fd1199fa00386e51c51c50b
student <123@mail.ru>
2021-12-02 15:12:09 +0300

    Initial commit 114

---
+++ app/src/main/java/com/example/dialog/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.dialog;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onCloseButtonClick(View view) {
+        
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Выход из приложения")
+                .setIcon(R.drawable.ic_launcher_foreground)
+                .setMessage("Вы уверены что хотите закрыть приложение?")
+                .setCancelable(false)
+                .setPositiveButton("ОК",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                finish();
+                            }
+                        }
+                )
+                .setNegativeButton("Отмена",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        }
+                );
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+}
